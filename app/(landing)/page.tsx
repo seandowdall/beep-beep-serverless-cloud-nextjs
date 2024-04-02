c1acbd3239820ebec4487da5feb4b5b5f92bf041
Sean Dowdall <seanied1234567@gmail.com>
2024-04-02 11:01:49 +0100

    copy chnaged

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -8,8 +8,8 @@ export default function Home() {
     <main>
       <LandingNavbar />
       <LandingSearch />
-      <div className="mt-10 pb-10 flex flex-col space-y-5">
-        <h1 className="text-3xl font-medium">Displaying All Cars:</h1>
+      <div className="p-10 flex flex-col space-y-5">
+        <h1 className="text-3xl font-medium">Available Cars:</h1>
         <DisplayAllCards />
       </div>
     </main>
