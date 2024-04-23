129354b5d679c6d7647744113f5f32ce7fe5fca8
Sean Dowdall <seanied1234567@gmail.com>
2024-04-23 13:53:09 +0100

    remove the landing search to be replaced by GSI

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -6,7 +6,7 @@ import DisplayAllCards from "./_components/display-all-cards";
 export default function Home() {
   return (
     <main>
-      <LandingSearch />
+      {/* <LandingSearch /> */}
       <div className="p-10 flex flex-col space-y-5">
         <h1 className="text-3xl font-medium">Available Cars:</h1>
         <DisplayAllCards />
