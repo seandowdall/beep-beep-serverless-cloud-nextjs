f97423f94a4eff5ce3d6feb1d5089305bac59f30
Sean Dowdall <seanied1234567@gmail.com>
2024-04-23 10:43:35 +0100

    small style changes

--- app/(main)/(routes)/profile/_components/booking-card.tsx
+++ app/(main)/(routes)/profile/_components/booking-card.tsx
@@ -1,6 +1,13 @@
 import React from "react";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"; // Import UI components
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"; // Import UI components
 import { Booking } from "@/types/types"; // Assuming Booking is a defined type
+import DeleteAlertDialog from "@/components/delete-alert-dialog";
 
 interface BookingCardProps {
   booking: Booking;
@@ -9,19 +16,16 @@ interface BookingCardProps {
 const BookingCard: React.FC<BookingCardProps> = ({ booking }) => {
   return (
     <div>
-      <Card className="mt-10">
-        <CardHeader>
-          <CardTitle>{/* {booking.make} {booking.model}  */}</CardTitle>
-        </CardHeader>
-        <CardContent>
+      <Card className="mt-3">
+        <CardContent className="mt-5">
           <ul>
-            <li>Booking ID: {booking.BookingID}</li>
-            <li>User ID: {booking.userID}</li>
-            <li>
+            <li className="text-xl mb-1">Booking ID: {booking.BookingID}</li>
+            <li className="text-xl mb-1">Email: {booking.userID}</li>
+            <li className="text-xl mb-1">
               Approval Status: {booking.isApproved ? "Approved" : "Pending"}
             </li>
-            <li>Start Date: {booking.startDate}</li>
-            <li>End Date: {booking.endDate}</li>
+            <li className="text-xl mb-1">Start Date: {booking.startDate}</li>
+            <li className="text-xl mb-1">End Date: {booking.endDate}</li>
             {/* <li>Total Cost: €{booking.totalCost}</li> */}
           </ul>
         </CardContent>
