390d6870e4335ec671b24ec1101bd38237f35eaf
Sean Dowdall <seanied1234567@gmail.com>
2024-04-23 11:28:10 +0100

    update to hopefully fix my deployment issues from next auth

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,15 +1,5 @@
-import NextAuth from "next-auth";
-import CognitoProvider from "next-auth/providers/cognito";
-
-export const authOptions = {
-  providers: [
-    CognitoProvider({
-      clientId: process.env.COGNITO_CLIENT_ID ?? " ",
-      clientSecret: process.env.COGNITO_CLIENT_SECRET ?? " ",
-      issuer: process.env.COGNITO_ISSUER,
-    }),
-  ],
-};
+import { authOptions } from "@/utils/authOptions";
+import NextAuth from "next-auth/next";
 
 export const handler = NextAuth(authOptions);
 
