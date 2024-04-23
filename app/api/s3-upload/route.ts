d26330e9909139e445590aeef18ccfe722fbbd09
Sean Dowdall <seanied1234567@gmail.com>
2024-04-23 12:05:48 +0100

    hopefully this results in less errors

--- app/api/s3-upload/route.ts
+++ app/api/s3-upload/route.ts
@@ -10,7 +10,7 @@ import {
 
 const Bucket = process.env.S3_BUCKET;
 const s3 = new S3Client({
-  region: process.env.AWS_REGION,
+  region: process.env.REGION,
   credentials: {
     accessKeyId: process.env.AWS_ACCESS_KEY_ID as string,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY as string,
